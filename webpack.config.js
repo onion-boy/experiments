f5932bb6056fbb002aca957d6f4b986196535221
onion-boy <24goldnere@brooklinek12.org>
2022-10-20 19:17:31 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.ts'
+    },
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        static: './dist',
+    }
+}
